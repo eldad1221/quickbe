ca5bb874c27f4b550b7e051083e52513eb32a794
Just Me 2 <justme2@mapo.me>
2022-10-25 23:05:04 +0300

    Version 3.3.0: Auto generating endpoints documentation

--- examples/run_web_server.py
+++ examples/run_web_server.py
@@ -18,16 +18,50 @@ def say_hello(session: HttpSession):
 
 
 @endpoint(validation={
-    'text': {'required': True, 'type': 'string'},
-    'name': {'required': False, 'type': 'string'},
-    'age': {'required': False, 'type': 'integer'}
+    'text': {
+        'required': True, 'type': 'string',
+        'doc': 'Some text to echo', 'example': 'Hello world'
+    },
+    'name': {
+        'required': False, 'type': 'string',
+        'doc': 'Name, just for testing', 'example': 'John Doe'
+    },
+    'age': {
+        'required': False, 'type': 'integer',
+        'doc': 'Persons age', 'example': 42
+    },
+    'object_info': {
+        'doc': 'Object attributes',
+        'type': 'dict',
+        'allow_unknown': False,
+        'required': True,
+        'schema': {
+            'lat': {'type': 'float', 'required': True},
+            'long': {'type': 'float', 'required': True},
+            'name': {'type': 'string', 'required': True},
+            'description': {'type': 'string', 'required': True},
+            'quantity': {'type': 'integer', 'default': -1},
+            'lifespan': {'type': 'integer', 'default': -1},
+            'attachments_url': {'type': 'list'},
+            'color': {'type': 'string', 'default': '#4285F4'},
+            'type': {'type': 'string', 'default': 'c0'},
+            'qr_code': {'type': 'boolean', 'default': False},
+        }
+    }
 }
 )
 def echo(session: HttpSession):
     return session.get('text')
 
 
-@endpoint(path='go', validation={'to': {'type': 'string', 'required': True}})
+@endpoint(path='go', validation={
+    'to': {
+        'type': 'string',
+        'required': True,
+        'doc': 'Url to redirect to',
+        'example': 'https://www.google.com'
+    }
+})
 def goto(session: HttpSession):
     url = session.get('to')
     Log.debug(f'Redirecting to {url}...')
